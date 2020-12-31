1d64361a37adc8c01636bc1f74592a3115daf9fd
dsaxena <deepak27april@gmail.com>
2020-12-31 15:50:39 +0530

    Function added

--- hungry.py
+++ hungry.py
@@ -1,8 +1,12 @@
-print("deepak")
-print("Swati")
-print("Mumbai")
-print("MP")
-print("Delhi")
-print("Football")
-print("Cricket")
+def getTotal(lst):
+    total=0
+    for item in lst:
+        total=total+item
+    return total
 
+l1={1,2,3,4,5}
+l2={11,22,33,44,55}
+totalL1=getTotal(l1)
+totalL2=getTotal(l2)
+print("Total of List 1:" , totalL1 )
+print("Total of List 2:" , totalL2 )
